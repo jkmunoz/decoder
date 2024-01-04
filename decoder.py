69b5fc75f62b2e2191e6fa87fb071c3a452eff8d
unknown <juliannakmunoz@gmail.com>
2024-01-04 15:20:37 -0600

    opens, reads, and prints file. printing strings incorrectly.

--- decoder.py
+++ decoder.py
@@ -1,8 +1,11 @@
+content = []
 # Function to decode the message.
 def decode(message_file):
-    content = open(message_file, 'r')
-    for line in content:
-        print(line)
+    # Open the given file and reads each line. 
+    text = open(message_file, 'r')
+    for line in text:
+        content.append(line)
+        print(content)
 
 # Example Usage
 message_file = 'coding_qual_input.txt'
