0c0d81f0fc840eb0d01a50722c96dac5717ed019
unknown <juliannakmunoz@gmail.com>
2024-01-07 00:14:51 -0600

    returns a list of strings. Each line of file is a string in the list.

--- decoder.py
+++ decoder.py
@@ -1,12 +1,19 @@
+lines_as_strings = []
+
 # Function to decode the message.
 def decode(message_file):
     # Open the given file and reads each line. 
     text = open(message_file, 'r')
     # Loops though and reads each line of text.
     for line in text:
-        # makes each line a list of two strings.
-        manyLines = line.split()
-        print(manyLines)
+        # # makes each line a list of two strings.
+        # manyLines = line.split()
+        # print(manyLines)
+
+        lines_as_strings.append(line.strip())
+        lines_as_strings.sort()
+    return lines_as_strings
+
 
 # Example Usage
 message_file = 'coding_qual_input.txt'
