f4f2d618eb3798e912684aa9960857b47511c139
unknown <juliannakmunoz@gmail.com>
2024-01-07 00:44:28 -0600

    Returns sorted list of strings. Each string item is a line from file.

--- decoder.py
+++ decoder.py
@@ -12,10 +12,21 @@ def decode(message_file):
 
         lines_as_strings.append(line.strip())
         lines_as_strings.sort()
-    return lines_as_strings
-
+    # Print statement here does not make a pyramid. Numbers are sorted but only by the first digit (ex: 9 is greater than 10).
+    print(lines_as_strings)
+        # print statement here makes a pyramid but code starts over with each line.
 
 # Example Usage
 message_file = 'coding_qual_input.txt'
 result = decode(message_file)
 print(result)
+
+
+# for line in text:
+#         # # makes each line a list of two strings.
+#         # manyLines = line.split()
+#         # print(manyLines)
+
+#         lines_as_strings.append(line.strip())
+#         # lines_as_strings.sort()
+#     print(lines_as_strings)
