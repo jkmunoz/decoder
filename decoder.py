1ecadbd41773823677fe1dadc77edec433188b0e
unknown <juliannakmunoz@gmail.com>
2024-01-11 12:46:53 -0600

    output is in the shape of Floyd's triangle but an error is also being thrown.

--- decoder.py
+++ decoder.py
@@ -10,9 +10,22 @@ def decode(message_file):
             lines_as_strings.append(line.strip())
     # Sorts lines by the first three characters
     sorted_lines = sorted(lines_as_strings, key=lambda x: int(x.split()[0]))
-    return sorted_lines
-        
+
+    current_pair_index = 0
+
+    # This is an outer loop to handle the # of ROWS.
+    for i in range(0, len(sorted_lines)):
+        # This is an inner loop to handle the # of COLUMNS.
+        for j in range(0, i + 1):
+            current_pair = sorted_lines[current_pair_index].split()
+            print(f"{current_pair[0]} {current_pair[1]}", end=' ')
+            current_pair_index += 1
+        print()
+
+
+    # print(sorted_lines)
 # Example Usage
 message_file = 'coding_qual_input.txt'
-result = decode(message_file)
-print(result)
+decode(message_file)
+
+
