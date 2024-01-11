435ac4c4c3c4a80138a398e16f24129ed2ee08d5
unknown <juliannakmunoz@gmail.com>
2024-01-11 10:41:00 -0600

    replaced print statement with return for better output

--- decoder.py
+++ decoder.py
@@ -5,28 +5,16 @@ def decode(message_file):
     # Open the given file and reads each line. 
     text = open(message_file, 'r')
     # Loops though and reads each line of text.
-    for line in text:
-        # # makes each line a list of two strings.
-        # manyLines = line.split()
-        # print(manyLines)
-
+    for line in text:   
         lines_as_strings.append(line.strip())
-        lines_as_strings.sort()
+        sorted_lines = sorted(lines_as_strings, key=lambda x: x[:3])
     # Print statement here does not make a pyramid. Numbers are sorted but only by the first digit (ex: 9 is greater than 10).
-    print(lines_as_strings)
+    # print(sorted_lines)
         # print statement here makes a pyramid but code starts over with each line.
-
+        # print(sorted_lines)
+    return sorted_lines
+        
 # Example Usage
 message_file = 'coding_qual_input.txt'
 result = decode(message_file)
 print(result)
-
-
-# for line in text:
-#         # # makes each line a list of two strings.
-#         # manyLines = line.split()
-#         # print(manyLines)
-
-#         lines_as_strings.append(line.strip())
-#         # lines_as_strings.sort()
-#     print(lines_as_strings)
